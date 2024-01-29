2933069147eec62cab37b3f3d809ac64be7c80f5
sn0w12 <lucasskold49@gmail.com>
2024-01-29 23:00:24 +0100

    Updated readme

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ A collection of nodes and improvements created mainly for testing loras and gene
 
 # Nodes
 
-## Find SDXL Resolution
-> An easy way to select a width and height, 
+## Find Resolution
+> An easy way to select a width and height.
 > <details>
 >    <summary>ℹ️ <i>See More Information</i></summary>
 >
@@ -28,3 +28,84 @@ A collection of nodes and improvements created mainly for testing loras and gene
 >    ![Find Resolution](./imgs/find_res.png)
 >    </details>
 
+## Lora Selector
+> Automatically selects X amount of loras between two numbers.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - Loras have to be formatted like the default kohya_ss outputs. (lora_name-000001)
+>    - Select the first lora.
+>    - Select the number of the highest lora you want to test.
+>    - Select the amount of loras you want to test.
+>    - Outputs list of loras like this: \<lora:name:strength>
+>    - Add default generation adds an extra "nothing" at the end of the list, used in Lora Tester to generate an image without the lora.
+> 
+>    ![Lora Selector](./imgs/lora_selector.png)
+>    </details>
+
+## Lora Tester (XL)
+> Automatically generates X amount of images with provided loras.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - I recommend making lora_info and add_default_generation inputs and using the outputs from the Lora Selector.
+>    - Takes normal KSampler input but takes positive and negative inputs as text.
+>    - Outputs a batch of images.
+> 
+>    ![Lora Tester](./imgs/lora_tester.png)
+>    </details>
+
+## Character Selector
+> Outputs a character name and prompt.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - Loads characters.json and outputs prompt based on it.
+>    - Note: Currently very small list, feel free to add your own to the json file.
+> 
+>    ![Character Selector](./imgs/character_selector.png)
+>    </details>
+
+## Prompt Combine
+> Combines 4 string to a single string.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - Combines 4 strings into one with a seperator, but only adds the seperator of there isnt one already at the end of the string.
+> 
+>    ![Prompt Combine](./imgs/prompt_combine.png)
+>    </details>
+
+## Lora Stacker
+> Outputs a list of loras for Lora Tester.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - Basically Lora Selector but manual.
+> 
+>    ![Lora Stacker](./imgs/lora_stacker.png)
+>    </details>
+
+## Load Lora XL/1.5
+> Normal load lora but from another folder.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - Add these to your extra_model_paths.yaml (for example):
+>    - loras_xl: C:/path/XL
+>    - loras_15: C:/path/1.5
+>    ![Load Lora](./imgs/load_lora.png)
+>    ![Example](./imgs/lora_paths_example.png)
+>    </details>
+
+## Split String
+> Splits a string into 4 parts.
+> <details>
+>    <summary>ℹ️ <i>See More Information</i></summary>
+>
+>    - Splits a string into 4 parts, not very useful.
+>
+>    ![Split String](./imgs/split_string.png)
+>    </details>
+
+# Example Workflows
--- src/character_select.py
+++ src/character_select.py
@@ -2,53 +2,33 @@ import os
 import json
 
 class CharacterSelectNode:
+    dir_path = os.path.dirname(os.path.realpath(__file__)).replace("\\src", "")
+    json_path = os.path.join(dir_path, 'characters.json')
+    with open(json_path, 'r') as file:
+        character_data = json.load(file)
+    character_dict = {character['name']: character for character in character_data}
+
     @classmethod
     def INPUT_TYPES(cls):
-        # Get the directory of the current script
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        dir_path = dir_path.replace("\\src", "")
-        # Construct the full path to the JSON file
-        json_path = os.path.join(dir_path, 'characters.json')
-
-        # Load character data from a JSON file
-        with open(json_path, 'r') as file:
-            character_data = json.load(file)
-
-        # Extracting only the character names for the input list
-        character_names = ['None'] + [character['name'] for character in character_data]
-
+        character_names = ['None'] + list(cls.character_dict.keys())
         return {
             "required": {
                 "character": (character_names, ),
-                "character_strength": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 100.0, "step":0.05, "round": 0.01}),
-                "character_pronpt": ("BOOLEAN", {"default": False},),
+                "character_strength": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 100.0, "step": 0.05, "round": 0.01}),
+                "character_pronpt": ("BOOLEAN", {"default": False}),
             },
         }
 
-    RETURN_TYPES = ("STRING","STRING",)
-    RETURN_NAMES = ("CHARACTER STRING","CHARACTER PROMPT",)
+    RETURN_TYPES = ("STRING", "STRING",)
+    RETURN_NAMES = ("CHARACTER STRING", "CHARACTER PROMPT",)
     FUNCTION = "find_associated_string"
     CATEGORY = "sn0w"
         
     def find_associated_string(self, character, character_strength, character_pronpt):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        dir_path = dir_path.replace("\\src", "")
-        json_path = os.path.join(dir_path, 'characters.json')
-
-        with open(json_path, 'r') as file:
-            character_data = json.load(file)
-
-        for char_item in character_data:
-            if char_item['name'] == character:
-                if character_strength == 1:
-                    if(character_pronpt):
-                        return (f"{char_item['associated_string']}, ", char_item['prompt'], )
-                    else:
-                        return (f"{char_item['associated_string']}, ", "", )
-                else:
-                    if(character_pronpt):
-                        return (f"({char_item['associated_string']}:{character_strength}), ", char_item['prompt'], )
-                    else:
-                        return (f"({char_item['associated_string']}:{character_strength}), ", "", )
-
+        char_item = self.character_dict.get(character)
+        if char_item:
+            associated_string = char_item['associated_string']
+            prompt = char_item['prompt'] if character_pronpt else ""
+            strength_part = f":{character_strength}" if character_strength != 1 else ""
+            return (f"({associated_string}{strength_part}), ", prompt)
         return (None,)  # Return a tuple if character not found
