dd2c595b98b888972e4e1c016b9d853a77e12168
Lucas  Sköld <lucasskold49@gmail.com>
2024-04-23 21:34:16 +0200

    Create loraFolderMinDistance.js

---
+++ web/loraFolderMinDistance.js
@@ -0,0 +1,23 @@
+import { app } from "../../../scripts/app.js";
+
+const id = "sn0w.LoraFolderMinDistance";
+const settingDefinition = {
+    id,
+    name: "[Sn0w] Max difference in lora loading",
+    defaultValue: 5,
+    min: 0,
+    max: 20,
+    step: 1,
+    type: "slider",
+};
+
+let setting;
+
+const extension = {
+    name: id,
+    init() {
+        setting = app.ui.settings.addSetting(settingDefinition);
+    },
+};
+
+app.registerExtension(extension);
