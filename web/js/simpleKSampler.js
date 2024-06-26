a9d9843452b14d34420ea837ef63f43b4f22400e
sn0w12 <lucasskold49@gmail.com>
2024-06-26 17:37:35 +0200

    Update name

--- web/js/simpleKSampler.js
+++ web/js/simpleKSampler.js
@@ -3,7 +3,7 @@ import { app } from "../../../scripts/app.js";
 app.registerExtension({
     name: "sn0w.SimpleKSampler",
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if (nodeData.name === "Simple KSampler") {
+        if (nodeData.name === "Sn0w KSampler") {
             const onConnectInput = nodeType.prototype.onConnectInput;
             nodeType.prototype.onConnectInput = function (targetSlot, type, output, originNode, originSlot) {
                 if (targetSlot === 3 || targetSlot === 4) {
