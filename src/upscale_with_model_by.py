39eec6877f7a0874e2028e86cc53d0fe40ad13a9
Lucas Sköld <lucasskold49@gmail.com>
2025-10-03 11:35:49 +0200

    Implement nodes_upscale_model.py workaround

--- src/upscale_with_model_by.py
+++ src/upscale_with_model_by.py
@@ -1,5 +1,91 @@
-from comfy_extras.nodes_upscale_model import ImageUpscaleWithModel
-from nodes import ImageScaleBy
+import torch
+import comfy.utils
+from comfy import model_management
+
+
+class _ImageUpscaleWithModel:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "upscale_model": ("UPSCALE_MODEL",),
+                "image": ("IMAGE",),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "upscale"
+
+    CATEGORY = "image/upscaling"
+
+    def upscale(self, upscale_model, image):
+        device = model_management.get_torch_device()
+
+        memory_required = model_management.module_size(upscale_model.model)
+        memory_required += (
+            (512 * 512 * 3) * image.element_size() * max(upscale_model.scale, 1.0) * 384.0
+        )  # The 384.0 is an estimate of how much some of these models take, TODO: make it more accurate
+        memory_required += image.nelement() * image.element_size()
+        model_management.free_memory(memory_required, device)
+
+        upscale_model.to(device)
+        in_img = image.movedim(-1, -3).to(device)
+
+        tile = 512
+        overlap = 32
+
+        oom = True
+        while oom:
+            try:
+                steps = in_img.shape[0] * comfy.utils.get_tiled_scale_steps(
+                    in_img.shape[3], in_img.shape[2], tile_x=tile, tile_y=tile, overlap=overlap
+                )
+                pbar = comfy.utils.ProgressBar(steps)
+                s = comfy.utils.tiled_scale(
+                    in_img,
+                    lambda a: upscale_model(a),
+                    tile_x=tile,
+                    tile_y=tile,
+                    overlap=overlap,
+                    upscale_amount=upscale_model.scale,
+                    pbar=pbar,
+                )
+                oom = False
+            except model_management.OOM_EXCEPTION as e:
+                tile //= 2
+                if tile < 128:
+                    raise e
+
+        upscale_model.to("cpu")
+        s = torch.clamp(s.movedim(-3, -1), min=0, max=1.0)
+        return (s,)
+
+
+class _ImageScaleBy:
+    upscale_methods = ["nearest-exact", "bilinear", "area", "bicubic", "lanczos"]
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "upscale_method": (s.upscale_methods,),
+                "scale_by": ("FLOAT", {"default": 1.0, "min": 0.01, "max": 8.0, "step": 0.01}),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "upscale"
+
+    CATEGORY = "image/upscaling"
+
+    def upscale(self, image, upscale_method, scale_by):
+        samples = image.movedim(-1, 1)
+        width = round(samples.shape[3] * scale_by)
+        height = round(samples.shape[2] * scale_by)
+        s = comfy.utils.common_upscale(samples, width, height, upscale_method, "disabled")
+        s = s.movedim(1, -1)
+        return (s,)
 
 
 class UpscaleImageBy:
@@ -34,8 +120,8 @@ class UpscaleImageBy:
         return factor_width
 
     def upscale(self, image, upscale_by, upscale_model):
-        upscaler = ImageUpscaleWithModel()
-        scale_image = ImageScaleBy()
+        upscaler = _ImageUpscaleWithModel()
+        scale_image = _ImageScaleBy()
 
         # Get original image size
         original_size = self.get_image_size(image)
